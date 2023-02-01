3af168045579a379d99b90496f7c886cc07390d4
Lizeth Rodriguez <lizeth.rodriguez@quantomm.tech>
2023-02-01 11:57:18 -0500

    Test blur formulario

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -1,29 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, InputLabel, NativeSelect, TextField } from "@mui/material";
 
 export const Form = () => {
+    const [formErrors, setFormErrors] = useState({
+        name: "",
+        size: "",
+        type: "",
+    });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { name, size, type } = e.target.elements;
+
+        if (!name.value) {
+            setFormErrors((prevState) => ({
+                ...formErrors,
+                name: "The name is required",
+            }));
+        }
+        if (!size.value) {
+            setFormErrors((prevState) => ({
+                ...prevState,
+                size: "The size is required",
+            }));
+        }
+
+        if (!type.value) {
+            setFormErrors((prevState) => ({
+                ...prevState,
+                type: "The type is required",
+            }));
+        }
+    };
+
+    const handleBlur = (e) => {
+        const { name, value } = e.target;
+
+        setFormErrors({
+            ...formErrors,
+            [name]: value.length ? "" : `The ${name} is required`,
+        });
+    };
     return (
         <>
             <h1>Create product</h1>
-            <form>
-                <TextField id="name" label="name"></TextField>
-                <TextField id="size" label="size"></TextField>
+
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="name"
+                    label="name"
+                    name="name"
+                    helperText={formErrors.name}
+                    onBlur={handleBlur}
+                ></TextField>
+                <TextField
+                    id="size"
+                    label="size"
+                    name="size"
+                    helperText={formErrors.size}
+                    onBlur={handleBlur}
+                ></TextField>
                 <InputLabel variant="standard" htmlFor="type">
                     Type
                 </InputLabel>
                 <NativeSelect
-                    defaultValue={30}
                     inputProps={{
                         name: "type",
                         id: "type",
                     }}
                 >
-                    <option value={10}>Electronic</option>
-                    <option value={20}>Furniture</option>
-                    <option value={30}>Clothing</option>
+                    <option value={""}></option>
+                    <option value={"electronic"}>Electronic</option>
+                    <option value={"furniture"}>Furniture</option>
+                    <option value={"clothing"}>Clothing</option>
                 </NativeSelect>
 
-                <Button>Submit</Button>
+                {formErrors.type.length && <p>{formErrors.type}</p>}
+
+                <Button type="submit">Submit</Button>
             </form>
         </>
     );
--- store-app/src/form/form.test.js
+++ store-app/src/form/form.test.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 import { Form } from "./form";
 
-describe("When the form is mounted", () => {
-    // eslint-disable-next-line testing-library/no-render-in-setup
-    beforeEach(() => render(<Form />));
+// eslint-disable-next-line testing-library/no-render-in-setup
+beforeEach(() => render(<Form />));
 
+describe("When the form is mounted", () => {
     test("There must be a create product form page", () => {
         // expect(screen.queryByText(/create product/i)).toBeInTheDocument();
         expect(
@@ -29,5 +29,45 @@ describe("When the form is mounted", () => {
         expect(
             screen.getByRole("button", { name: /submit/i })
         ).toBeInTheDocument();
+    });
+});
+
+describe("When the user submit the form whitout values", () => {
+    it("should display validation msj", () => {
+        expect(
+            screen.queryByText(/The name is required/i)
+        ).not.toBeInTheDocument();
+        expect(
+            screen.queryByText(/The size is required/i)
+        ).not.toBeInTheDocument();
+        expect(
+            screen.queryByText(/The type is required/i)
+        ).not.toBeInTheDocument();
+
+        fireEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+        expect(screen.getByText(/The name is required/i)).toBeInTheDocument();
+        expect(screen.getByText(/The size is required/i)).toBeInTheDocument();
+        expect(screen.getByText(/The type is required/i)).toBeInTheDocument();
+    });
+});
+
+describe("When the user blurs an empty field", () => {
+    it("should display validation error msj for the input name", () => {
+        fireEvent.blur(screen.getByLabelText(/name/i), {
+            target: { name: "name", value: "" },
+        });
+
+        // eslint-disable-next-line testing-library/prefer-presence-queries
+        expect(screen.queryByText(/the name is required/i)).toBeInTheDocument();
+    });
+
+    it("should display validation error msj for the input size", () => {
+        fireEvent.blur(screen.getByLabelText(/size/i), {
+            target: { name: "size", value: "" },
+        });
+
+        // eslint-disable-next-line testing-library/prefer-presence-queries
+        expect(screen.queryByText(/the size is required/i)).toBeInTheDocument();
     });
 });
