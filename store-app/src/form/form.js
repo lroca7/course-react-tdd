ab49185a9fb236b3575dbb903608d802c6678747
Lizeth Rodriguez <lizeth.rodriguez@quantomm.tech>
2023-02-01 10:34:51 -0500

    Prueba existencia de campos en el formulario

--- store-app/src/form/form.js
+++ store-app/src/form/form.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button, InputLabel, NativeSelect, TextField } from "@mui/material";
 
 export const Form = () => {
-    return <h1>create product</h1>;
+    return (
+        <>
+            <h1>Create product</h1>
+            <form>
+                <TextField id="name" label="name"></TextField>
+                <TextField id="size" label="size"></TextField>
+                <InputLabel variant="standard" htmlFor="type">
+                    Type
+                </InputLabel>
+                <NativeSelect
+                    defaultValue={30}
+                    inputProps={{
+                        name: "type",
+                        id: "type",
+                    }}
+                >
+                    <option value={10}>Electronic</option>
+                    <option value={20}>Furniture</option>
+                    <option value={30}>Clothing</option>
+                </NativeSelect>
+
+                <Button>Submit</Button>
+            </form>
+        </>
+    );
 };
 
 export default Form;
--- store-app/src/form/form.test.js
+++ store-app/src/form/form.test.js
@@ -5,12 +5,29 @@ import { render, screen } from "@testing-library/react";
 import { Form } from "./form";
 
 describe("When the form is mounted", () => {
-    test("There must be a create product form page", () => {
-        render(<Form />);
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    beforeEach(() => render(<Form />));
 
+    test("There must be a create product form page", () => {
         // expect(screen.queryByText(/create product/i)).toBeInTheDocument();
         expect(
             screen.getByRole("heading", { name: /create product/i })
+        ).toBeInTheDocument();
+    });
+
+    it("should exists the fields: name, size, type(electronic, furniture, clothing)", () => {
+        expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/size/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/type/i)).toBeInTheDocument();
+
+        expect(screen.getByText(/electronic/i)).toBeInTheDocument();
+        expect(screen.getByText(/furniture/i)).toBeInTheDocument();
+        expect(screen.getByText(/clothing/i)).toBeInTheDocument();
+    });
+
+    it("should exists the submit button", () => {
+        expect(
+            screen.getByRole("button", { name: /submit/i })
         ).toBeInTheDocument();
     });
 });
